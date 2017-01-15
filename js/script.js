431f2c6d54e7c2853e2f6b4a079245363408617b
rdalin82 <Robert.Dalin@gmail.com>
2017-01-15 11:56:40 -0600

    converted camel case to snake case

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
       var that = this;
       $(that).text(game.turn);
       var space = $(this).data("value");
-      game = new_board(space, game);
+      game = newBoard(space, game);
       ai.minmax(game);
       console.log("game turn:  " + game.turn);
       $('div').find("[data-value="+ai.choice.toString()+"]").text(game.turn);
-      game = new_board(ai.choice, game);
+      game = newBoard(ai.choice, game);
       if ( over(game) ){
         if (won(game, "x")) {
           $("#over").append("x won! click here for new game").addClass("border");
@@ -41,12 +41,12 @@ function won(game, player){
   var horizontals = [ [0, 1, 2] ,[3, 4, 5], [6, 7, 8] ];
   var verticals = [ [0, 3, 6], [1, 4, 7], [2, 5, 8] ];
   var diagonals = [ [0, 4, 8], [2, 4, 6] ];
-  var winning_moves = [horizontals, verticals, diagonals];
-  for (var group=0; group < winning_moves.length; group++){
-    for (var move=0; move < winning_moves[group].length; move++){
-      var set = winning_moves[group][move];
+  var winningMoves = [horizontals, verticals, diagonals];
+  for (var group=0; group < winningMoves.length; group++){
+    for (var move=0; move < winningMoves[group].length; move++){
+      var set = winningMoves[group][move];
       var check = [ board[set[0]], board[set[1]], board[set[2]] ];
-      if ( equal_arrays(check, [player, player, player]) ) {
+      if ( equalArrays(check, [player, player, player]) ) {
         return true;
       }
     }
@@ -54,7 +54,7 @@ function won(game, player){
   return false;
 }
 
-function equal_arrays(one, two){
+function equalArrays(one, two){
   if (one.length != two.length){
     return false;
   }
@@ -69,7 +69,7 @@ function equal_arrays(one, two){
 function over(game){
   if (won(game, "x") || won(game, "o")){
     return true;
-  } else if (!spaces_left(game)){
+  } else if (!spacesLeft(game)){
     return true;
   }
   else {
@@ -77,7 +77,7 @@ function over(game){
   }
 }
 
-function spaces_left(game){
+function spacesLeft(game){
   var board = game.board;
   for (var i = 0; i < board.length;i++){
     if (board[i]==undefined){
@@ -87,7 +87,7 @@ function spaces_left(game){
   return false; 
 }
 
-function availabe_moves(game){
+function availableMoves(game){
   var board = game.board;
   var spaces = [];
   for (var i =0; i < board.length; i++){
@@ -108,7 +108,7 @@ function score(game){
   }
 }
 
-function new_board(move, game){
+function newBoard(move, game){
   var board = game.board.slice();
   board[move] = game.turn;
   var turn;
@@ -131,20 +131,20 @@ var ai = {
     }
     var moves =[];
     var scores = [];
-    availabe_moves(game).forEach(function(move){
-      nBoard = new_board(move, game);
+    availableMoves(game).forEach(function(move){
+      nBoard = newBoard(move, game);
       scores.push(ai.minmax(nBoard));
       moves.push(move);
     });
 
     if (game.turn == "o"){
-      var max_index = scores.indexOf(scores.max());
-      ai.choice = moves[max_index]
-      return scores[max_index]
+      var maxIndex = scores.indexOf(scores.max());
+      ai.choice = moves[maxIndex]
+      return scores[maxIndex]
     } else {
-      var min_index = scores.indexOf(scores.min());
-      ai.choice = moves[min_index]
-      return scores[min_index]
+      var minIndex = scores.indexOf(scores.min());
+      ai.choice = moves[minIndex]
+      return scores[minIndex]
     }
   }
 }
